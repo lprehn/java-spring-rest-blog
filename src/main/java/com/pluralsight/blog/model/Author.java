d56d06c1627556b2a35f1d4d937d398ad5a72e2c
lprehn <35541263+lprehn@users.noreply.github.com>
2021-05-30 16:55:40 -0700

    Add Author Relationships and Data

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -18,11 +18,17 @@ public class Author {
     private Long id;
     private String firstname;
     private String lastname;
+    @JsonIgnore
     private String username;
+    @JsonIgnore
     private String password;
+    @OneToMany
+    private List<Post> posts;
 
-    public Author() {
+    public Author()
+    {
         super();
+        posts = new ArrayList<>();
     }
 
     public Author(String username, String firstname, String lastname, String password) {
@@ -84,11 +90,12 @@ public class Author {
         return true;
     }
 
-    public List<Post> getPosts() {
-        return null;
+    public List<Post> getPosts()
+    {
+        return posts;
     }
 
     public void addPost(Post post) {
-        return;
+        posts.add(post);
     }
 }
